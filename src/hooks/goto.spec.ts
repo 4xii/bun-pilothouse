170898fd87b5aa431925ef3e13815347147f4bbe
4xii <975795875@qq.com>
2023-11-22 12:23:04 +0800

    feat: add goto hook test and define route names

---
+++ src/hooks/goto.spec.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it, vi } from 'vitest'
+import {useGoto } from './goto'
+import { useSetup } from '@/tests/helper'
+import { RouteNames } from '@/router/const'
+
+describe('goto', () => {
+  it('should be go to Main', () => {
+    const { router } = useSetup(() => {
+      const { gotoMain } = useGoto()
+      gotoMain()
+    })
+
+    expect(router.push).toBeCalledWith({ name: RouteNames.MAIN })
+  })
+})
